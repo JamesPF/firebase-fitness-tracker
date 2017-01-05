7d07bbdc1e849cf13e157105ff341c17a84ffc33
James Felz <james@still-water.com>
2017-01-05 14:45:06 -0800

    Refactor code for initial load of line and area

--- public/js/app.js
+++ public/js/app.js
@@ -10,84 +10,84 @@ var parsedDate = d3.timeParse('%Y-%m-%d');
 var tooltipDate = d3.timeFormat('%b %e, %Y');
 
 // Creates canvas
-var canvas = d3.select('#chart')
-                .append('svg')
-                .attr('height', height)
-                .attr('width', width)
-                .style('background', '#ECF0F1')
-                .style('display', 'block')
-                .style('margin', '0 auto')
-                .append('g')
-                  .attr('transform', 'translate (90, -30)');
+var canvas = d3.select('svg')
+  .attr('height', height)
+  .attr('width', width)
+  .style('background', '#ECF0F1')
+  .style('display', 'block')
+  .style('margin', '0 auto')
+  .append('g')
+    .attr('transform', 'translate (90, -30)');
 
 var x = d3.scaleTime()
-          .domain(d3.extent(measurementArray, function (d) {
-            var date = parsedDate(d.date);
-            return date;
-          }))
-          .range([0, (width - (3.5 * margin))]);
+  .domain(d3.extent(measurementArray, function (d) {
+    var date = parsedDate(d.date);
+    return date;
+  }))
+  .range([0, (width - (3.5 * margin))]);
 
 var y = d3.scaleLinear()
-          .domain([0, d3.max(measurementArray, function (d) {
-            return d.weight;
-          })])
-          .range([(height - (1.5*margin)), margin]);
+  .domain([0, d3.max(measurementArray, function (d) {
+    return d.weight;
+  })])
+  .range([(height - (1.5*margin)), margin]);
 
 // Creates axes on using the x and y scales above
 // Also creates grid lines that span the width and height of the graph
 var xAxis = d3.axisBottom()
-            .scale(x)
-            .ticks(d3.timeMonth)
-            .tickFormat(d3.timeFormat('%b %Y'))
-            .tickSizeInner(-(height - (2.5*margin)))
-            .tickSizeOuter(0);
+  .scale(x)
+  .ticks(d3.timeMonth)
+  .tickFormat(d3.timeFormat('%b %Y'))
+  .tickSizeInner(-(height - (2.5*margin)))
+  .tickSizeOuter(0);
+
 var yAxis = d3.axisLeft()
-            .scale(y)
-            .ticks(d3.max(measurementArray, function (d) {
-              return d.weight/10;
-            }))
-            .tickSizeInner(-(width - (3.5 * margin)));
+  .scale(y)
+  .ticks(d3.max(measurementArray, function (d) {
+    return d.weight/10;
+  }))
+  .tickSizeInner(-(width - (3.5 * margin)));
 
 // Sets label on y axis
 canvas.append('text')
-      .attr('text-anchor', 'middle')
-      .attr('transform', 'translate(-35,' + (height / 2) + ')rotate(-90)')
-      .attr('font-family', 'Helvetica')
-      .attr('fill', '#222')
-      .text('Weight');
+  .attr('text-anchor', 'middle')
+  .attr('transform', 'translate(-35,' + (height / 2) + ')rotate(-90)')
+  .attr('font-family', 'Helvetica')
+  .attr('fill', '#222')
+  .text('Weight');
 
 // Sets label on x axis
 canvas.append('text')
-      .attr('text-anchor', 'middle')
-      .attr('transform', 'translate(' + ((width - (4 * margin)) / 2) + ',' + (height + 10) + ')')
-      .attr('font-family', 'Helvetica')
-      .attr('fill', '#222')
-      .text('Date');
+  .attr('text-anchor', 'middle')
+  .attr('transform', 'translate(' + ((width - (4 * margin)) / 2) + ',' + (height + 10) + ')')
+  .attr('font-family', 'Helvetica')
+  .attr('fill', '#222')
+  .text('Date');
 
 // Creates div for tooltip
 var div = d3.select('#chart')
-              .append('div')
-              .attr('class', 'tooltip')
-              .style('opacity', 0);
+  .append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
 
 // Generates the fill area beneath the data line
 var area = d3.area()
-             .x(function (d) {
-              var date = parsedDate(d.date);
-              return x(date);
-             })
-             .y0(height - (1.5*margin))
-             .y1(function (d) {return y(d.weight);})
-             .curve(d3.curveMonotoneX);
+  .x(function (d) {
+    var date = parsedDate(d.date);
+    return x(date);
+  })
+  .y0(height - (1.5*margin))
+  .y1(function (d) {return y(d.weight);})
+  .curve(d3.curveMonotoneX);
 
 // Generates the lines
 var line = d3.line()
-            .x(function (d) {
-              var date = parsedDate(d.date);
-              return x(date);
-            })
-            .y(function (d) {return y(d.weight);})
-            .curve(d3.curveMonotoneX);
+  .x(function (d) {
+    var date = parsedDate(d.date);
+    return x(date);
+  })
+  .y(function (d) {return y(d.weight);})
+  .curve(d3.curveMonotoneX);
 
 function buildChart (measurementArray) {
   console.log('before', measurementArray);
@@ -100,48 +100,63 @@ function buildChart (measurementArray) {
   // Draws graph
 
   x = d3.scaleTime()
-          .domain(d3.extent(measurementArray, function (d) {
-            var date = parsedDate(d.date);
-            return date;
-          }))
-          .range([0, (width - (3.5 * margin))]);
+    .domain(d3.extent(measurementArray, function (d) {
+      var date = parsedDate(d.date);
+      return date;
+    }))
+    .range([0, (width - (3.5 * margin))]);
 
   y = d3.scaleLinear()
-          .domain([0, d3.max(measurementArray, function (d) {
-            return d.weight;
-          })])
-          .range([(height - (1.5*margin)), margin]);
+    .domain([0, d3.max(measurementArray, function (d) {
+      return d.weight;
+    })])
+    .range([(height - (1.5*margin)), margin]);
 
   xAxis = d3.axisBottom()
-              .scale(x)
-              .ticks(d3.timeMonth)
-              .tickFormat(d3.timeFormat('%b %Y'))
-              .tickSizeInner(-(height - (2.5*margin)))
-              .tickSizeOuter(0);
+    .scale(x)
+    .ticks(d3.timeMonth)
+    .tickFormat(d3.timeFormat('%b %Y'))
+    .tickSizeInner(-(height - (2.5*margin)))
+    .tickSizeOuter(0);
 
   yAxis = d3.axisLeft()
-              .scale(y)
-              .ticks(d3.max(measurementArray, function (d) {
-                return d.weight/10;
-              }))
-              .tickSizeInner(-(width - (3.5 * margin)));
+    .scale(y)
+    .ticks(d3.max(measurementArray, function (d) {
+      return d.weight/10;
+    }))
+    .tickSizeInner(-(width - (3.5 * margin)));
 
-  // Creates the fill area beneath the line
-  canvas.append('path')
-        .data([measurementArray])
-        .attr('class', 'area')
-        .attr('d', area);
+  // Creates the shaded area underneath the line
+  var newArea = canvas.selectAll('area')
+    .data([measurementArray]);
 
-  // Creates lines, and uses '.path' as selector to differentiate 'path' from the area creator above
-  canvas.selectAll('.path')
-          .data([measurementArray])
-          .enter()
-          .append('path')
-            .attr('class', '.path')
-            .attr('d', line)
-            .attr('fill', 'none')
-            .attr('stroke', '#2980B9')
-            .attr('stroke-width', 3);
+  newArea.attr('d', area)
+    .attr('class', 'area')
+    .enter()
+      .append('path')
+      .attr('d', area)
+      .attr('class', 'area');
+
+  newArea.exit().remove();
+
+  // Creates the line
+  var newLine = canvas.selectAll('.path')
+    .data([measurementArray]);
+
+  newLine.attr('d', line)
+    .attr('class', 'path')
+    .attr('fill', 'none')
+    .attr('stroke', '#2980B9')
+    .attr('stroke-width', 3)
+    .enter()
+      .append('path')
+      .attr('class', '.path')
+      .attr('d', line)
+      .attr('fill', 'none')
+      .attr('stroke', '#2980B9')
+      .attr('stroke-width', 3);
+
+  newLine.exit().remove();
 
   // Appends x axis and moves it to bottom
   canvas.append('g')
